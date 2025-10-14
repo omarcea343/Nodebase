b698536cedec09a3dfd289b0ab1e4f8fe46dddf9
Omar Zuniga <miguel_omar30@hotmail.com>
2025-10-13 23:11:06 -0600

    Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+"use client";
 
-const Page = async () => {
-	await requireAuth();
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-	const data = await caller.getUsers();
+const Page = () => {
+	const trpc = useTRPC();
+	const queryClient = useQueryClient();
+	const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+	const create = useMutation(
+		trpc.createWorkflow.mutationOptions({
+			onSuccess: () => {
+				queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+			},
+		})
+	);
 
 	return (
-		<div className="min-h-screen min-w-screen flex items-center justify-center">
-			{JSON.stringify(data, null, 2)}
+		<div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+			<div>{JSON.stringify(data, null, 2)}</div>
+			<Button disabled={create.isPending} onClick={() => create.mutate()}>
+				Create Workflow
+			</Button>
 		</div>
 	);
 };
