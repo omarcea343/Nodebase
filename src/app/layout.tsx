91fc9a2dbcc816c448526c265eb7af73f549e79a
Omar Zuniga <miguel_omar30@hotmail.com>
2025-10-21 23:19:29 -0600

    Workflows Pagination

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<TRPCReactProvider>
-					{children}
-					<Toaster richColors />
+					<NuqsAdapter>
+						{children}
+						<Toaster richColors />
+					</NuqsAdapter>
 				</TRPCReactProvider>
 			</body>
 		</html>
--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { EntityContainer, EntityHeader } from "@/components/entity-components";
-import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows";
+import { EntityContainer, EntityHeader, EntityPagination, EntitySearch } from "@/components/entity-components";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
+import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows";
+import { useWorkflowsParams } from "../hooks/use-workflows-params";
+import { useEntitySearch } from "@/hooks/use-entity-search";
+
+export const WorkflowsSearch = () => {
+	const [params, setParams] = useWorkflowsParams();
+	const { searchValue, onSearchChange } = useEntitySearch({
+		params,
+		setParams,
+	});
+
+	return <EntitySearch value={searchValue} onChange={onSearchChange} placeholder="Search workflows" />;
+};
 
 export const WorkflowsList = () => {
 	const workflows = useSuspenseWorkflows();
@@ -46,9 +58,23 @@ export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
 	);
 };
 
+export const WorkflowsPagination = () => {
+	const workflows = useSuspenseWorkflows();
+	const [params, setParams] = useWorkflowsParams();
+
+	return (
+		<EntityPagination
+			disabled={workflows.isFetching}
+			totalPages={workflows.data.totalPages}
+			page={workflows.data.page}
+			onPageChange={(page) => setParams({ ...params, page })}
+		/>
+	);
+};
+
 export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<EntityContainer header={<WorkflowsHeader />} search={<></>} pagination={<></>}>
+		<EntityContainer header={<WorkflowsHeader />} search={<WorkflowsSearch />} pagination={<WorkflowsPagination />}>
 			{children}
 		</EntityContainer>
 	);
