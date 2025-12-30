6e3453ac938c86ddf2a529189e487329368de44f
Omar Zuniga <miguel_omar30@hotmail.com>
2025-12-30 13:42:43 -0600

    AI Providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,20 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { LogoutButton } from "./logout";
+import { toast } from "sonner";
 
 const Home = () => {
 	const trpc = useTRPC();
 	const queryClient = useQueryClient();
 	const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+	const testAi = useMutation(
+		trpc.testAI.mutationOptions({
+			onSuccess: () => {
+				toast.success("AI Job queued successfully");
+			},
+		})
+	);
 
 	const create = useMutation(
 		trpc.createWorkflow.mutationOptions({
@@ -21,6 +30,9 @@ const Home = () => {
 	return (
 		<div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
 			<div>{JSON.stringify(data, null, 2)}</div>
+			<Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+				Test AI
+			</Button>
 			<Button disabled={create.isPending} onClick={() => create.mutate()}>
 				Create Workflow
 			</Button>
