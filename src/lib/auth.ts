d36307fb9cc75beb896c2351a2c4ee538d4e1381
Omar Zuniga <miguel_omar30@hotmail.com>
2026-02-19 17:01:03 -0600

    feat: setup better-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import prisma from "./prisma";
+
+const database = prismaAdapter(prisma, {
+	provider: "postgresql",
+});
+
+export const auth = betterAuth({
+	database,
+	emailAndPassword: {
+		enabled: true,
+	},
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 60 * 5, // 5 minutes
+		},
+	},
+	plugins: [nextCookies()],
+});
